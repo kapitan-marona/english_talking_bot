524da10755bcf52c822bbcf2dd463f6be7f5777e
kapitan-marona <kapitanmarona@gmail.com>
2025-07-22 01:51:07 +0300

    english_bot.py

--- english_bot.py
+++ english_bot.py
@@ -11,14 +11,19 @@ def main():
     app = ApplicationBuilder().token(TOKEN).build()
 
     conv_handler = ConversationHandler(
-        entry_points=[CommandHandler("start", start)],
-        states={
-            LANG: [MessageHandler(filters.TEXT & ~filters.COMMAND, lang_choice)],
-            LEVEL: [MessageHandler(filters.TEXT & ~filters.COMMAND, level_choice)],
-            STYLE: [MessageHandler(filters.TEXT & ~filters.COMMAND, style_choice)],
-        },
-        fallbacks=[CommandHandler("cancel", cancel)],
-    )
+    entry_points=[CommandHandler("start", start)],
+    states={
+        DEVICE_LANG: [
+            MessageHandler(filters.Regex("^Use device language$"), device_lang_choice),
+            MessageHandler(filters.Regex("^Choose manually$"), manual_lang_choice),
+        ],
+        LANG: [MessageHandler(filters.TEXT & ~filters.COMMAND, lang_choice)],
+        LEARN_LANG: [MessageHandler(filters.TEXT & ~filters.COMMAND, learn_lang_choice)],
+        LEVEL: [MessageHandler(filters.TEXT & ~filters.COMMAND, level_choice)],
+        STYLE: [MessageHandler(filters.TEXT & ~filters.COMMAND, style_choice)],
+    },
+    fallbacks=[CommandHandler("cancel", cancel)],
+)
 
     app.add_handler(conv_handler)
     app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, chat))
