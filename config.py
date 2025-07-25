42ba2c42c702c60082e40ee29b2501623334deb8
kapitan-marona <kapitanmarona@gmail.com>
2025-07-25 10:04:34 +0300

    Update config.py

--- config.py
+++ config.py
@@ -5,6 +5,8 @@ from openai import OpenAI
 load_dotenv()
 
 TELEGRAM_TOKEN = os.getenv("TELEGRAM_TOKEN")
+WEBHOOK_SECRET_PATH = os.getenv("WEBHOOK_SECRET_PATH")
 OPENAI_API_KEY = os.getenv("OPENAI_API_KEY")
+
 
 client = OpenAI(api_key=OPENAI_API_KEY)
